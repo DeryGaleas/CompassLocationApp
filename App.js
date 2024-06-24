58e85325f60d0466381a6a6da86feb67fff2fbd8
Dery Galeas <dery.reyna@gmail.com>
2024-06-23 22:36:22 -0600

    First Version

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Compass from "./HomeScreen";
+import Location from "./Location";
 
+const Tab = createBottomTabNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={Compass} />
+        <Tab.Screen name="Location" component={Location} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ HomeScreen.jsx
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from "react";
+import { View, Image, StyleSheet, Text } from "react-native";
+import { Magnetometer } from "expo-sensors";
+
+const Compass = () => {
+  const [heading, setHeading] = useState(0);
+
+  useEffect(() => {
+    Magnetometer.setUpdateInterval(1000); // Update every 1 second
+
+    const subscription = Magnetometer.addListener((data) => {
+      const angle = calculateHeading(data.x, data.y);
+      setHeading(angle);
+    });
+
+    return () => subscription.remove();
+  }, []);
+
+  const calculateHeading = (x, y) => {
+    console.log(x, y);
+    let heading = Math.atan(y / x);
+    heading = heading * (180 / Math.PI); // Convert to degrees
+    if (x < 0) {
+      heading += 180;
+    } else if (y < 0) {
+      heading += 360;
+    }
+    return Math.round(heading);
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text>{heading}</Text>
+      <Image
+        source={require("./assets/compass.png")}
+        style={[
+          styles.compass,
+          { transform: [{ rotate: `${360 - heading}deg` }] },
+        ]}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  compass: {
+    width: 300,
+    height: 300,
+  },
+});
+
+export default Compass;
